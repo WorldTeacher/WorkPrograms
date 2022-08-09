f88b1bd01ea1b5cd6aaee57e4d99eef0b0606196
WorldTeacher <41587052+WorldTeacher@users.noreply.github.com>
2022-08-09 07:33:27 +0200

    files

--- Python/BWLastCopies/logger.py
+++ Python/BWLastCopies/logger.py
@@ -1,21 +1,30 @@
 #create logger
 import logging
-import logging.handlers
 import os
 
 #make sure the log directory exists
-if not os.path.exists('log'):
-    os.makedirs('log')
+#get path
+path=os.path.dirname(os.path.abspath(__file__))
+#if log directory not in path, add it
+if 'log' not in path:
+    path=path+'\log'    
+#if log directory does not exist, create it
+if not os.path.exists(path):
+    os.makedirs(path)
+
 class log:
     def __init__(self):
         #create logger for api
+        path=os.path.dirname(os.path.abspath(__file__))
+        path=path+'\log'
+        path=path+'\\'
         self.logger = logging.getLogger('API')
         self.logger.setLevel(logging.DEBUG)
         #create handler
-        self.handler = logging.FileHandler('log/api_log.log')
+        self.handler = logging.FileHandler(path +'api_log.log')
         self.handler.setLevel(logging.DEBUG)
         #create formatter
-        formatter = logging.Formatter("%(asctime)s;%(levelname)s;%(message)s",
+        formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s", 
                               "%Y-%m-%d %H:%M:%S")
         self.handler.setFormatter(formatter)
         #add handler to logger
@@ -24,14 +33,26 @@ class log:
         self.logger2 = logging.getLogger('General')
         self.logger2.setLevel(logging.DEBUG)
         #create handler
-        self.handler2 = logging.FileHandler('log/general_log.log')
+        self.handler2 = logging.FileHandler(path +'general_log.log')
         self.handler2.setLevel(logging.DEBUG)
         #create formatter
-        formatter2 = logging.Formatter("%(asctime)s;%(levelname)s;%(message)s",
+        formatter2 = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s",
                               "%Y-%m-%d %H:%M:%S")
         self.handler2.setFormatter(formatter2)
         #add handler to logger
         self.logger2.addHandler(self.handler2)
+        #create logger for the Database
+        self.logger3 = logging.getLogger('Database')
+        self.logger3.setLevel(logging.DEBUG)
+        #create handler
+        self.handler3 = logging.FileHandler(path +'database_log.log')
+        self.handler3.setLevel(logging.DEBUG)
+        #create formatter
+        formatter3 = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s",
+                                "%Y-%m-%d %H:%M:%S")
+        self.handler3.setFormatter(formatter3)
+        #add handler to logger
+        self.logger3.addHandler(self.handler3)
 
     def info_api(self,message):
         self.logger.info(message)
@@ -57,3 +78,16 @@ class log:
         self.logger2.critical(message)
     def fatal_general(self,message):
         self.logger2.fatal(message)
+    def info_database(self,message):
+        self.logger3.info(message)
+    def error_database(self,message):
+        self.logger3.error(message)
+    def debug_database(self,message):
+        self.logger3.debug(message)
+    def warning_database(self,message):
+        self.logger3.warning(message)
+    def critical_database(self,message):
+        self.logger3.critical(message)
+    def fatal_database(self,message):
+        self.logger3.fatal(message)
+
--- Python/BWLastCopies/variables.py
+++ Python/BWLastCopies/variables.py
@@ -4,7 +4,7 @@ import glob
 import logger
 #find json file
 jsonfile=glob.glob('C:\\Users\\aky547\\GitHub\\WorkPrograms\\Python\\BWLastCopies\\config.json',recursive=False)
-print(jsonfile[0])
+#print(jsonfile[0])
 #open config file
 log=logger.log()
 errmsg_gen="Error, check log/general_log.log for details"
@@ -16,7 +16,15 @@ try:
 except:
     log.warning_general("Error: Config file not Found, check path")
     print(errmsg_gen)
-    
+    exit()
+#set variables
+
+bibid=data['bibid']
+
+api_url_general='https://sru.k10plus.de/opac-de-627!rec=1?version=1.1&operation=searchRetrieve&query=pica.tit%3D{title}+and+pica.all%3D{author}&maximumRecords=10&recordSchema=marcxml'
+api_url_personal='https://sru.k10plus.de/opac-de-627!rec=1?version=1.1&operation=searchRetrieve&query=pica.tit%3D{title}+and+pica.all%3D{author}+and+pica.bib%3D{bibid}&maximumRecords=10&recordSchema=marcxml'
+##insert bibid into api_url_personal
+api_url_personal=api_url_personal.replace("{bibid}",bibid)
 #with open(jsonfile[0]) as config_file:
 #  data=json.load(config_file)
 
@@ -28,3 +36,4 @@ namespaces={  # Manually extracted from the XML file, but there could be code wr
             "zs": "http://www.loc.gov/zing/srw/",
             "": "http://www.loc.gov/MARC21/slim",
                 }
+database_path=data['Database']['Path']
