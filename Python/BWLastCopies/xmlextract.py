c7784102fbf35725093346528b40b2cf15ca0c9d
WorldTeacher <41587052+WorldTeacher@users.noreply.github.com>
2022-08-12 10:03:12 +0200

    added testfile, xmlextract update

--- Python/BWLastCopies/xmlextract.py
+++ Python/BWLastCopies/xmlextract.py
@@ -5,57 +5,46 @@ import lxml
 xml_file="./Python/BWLastCopies/test.xml"
 with open(xml_file,encoding='utf-8') as f:
     data=f.readlines()
-
+xmldata=[]
 for line in data:
-    #print(line)
+    x_data={'title':[],'author':[],'issue':[],'DE-640':[]}
     soup=BeautifulSoup(line,"lxml")
-    #get the title
     title_data=soup.find("datafield",{"tag":"245"})
     soup1=BeautifulSoup(str(title_data),"lxml")
     title_field=soup1.find("subfield",{"code":"a"})
     soup1=BeautifulSoup(str(title_data),"lxml")
     title_field=soup1.find("subfield",{"code":"a"})
     title=title_field.text
-    #print(title_field.text)
-    #get the author
+    x_data['title']=title
     try:
         author_data=soup.find("datafield",{"tag":"100"})
         soup2=BeautifulSoup(str(author_data),"lxml")
         author_field=soup2.find("subfield",{"code":"a"})
         author=author_field.text
-       #print(author_field.text)
+        x_data['author']=author
     except:
         author="no author"
-        #print(author)
-    #get the issues
+        x_data['author']=author
+
     try:
         issues_data=soup.find("datafield",{"tag":"250"})
         soup3=BeautifulSoup(str(issues_data),"lxml")
         issues_field=soup3.find("subfield",{"code":"a"})
-        #print(issues_field)
         issues=issues_field.text
+        x_data['issue']=issues
     except:
         issues="no issues"
-        #print(issues)
-    print('Title: '+title+' Author: '+author +' Issues: '+issues)
-    '''if title_field is not None:
-        title=title_field.text
-    else:
-        tags=["2","3","4","5","6"]
-        for tag in tags:
-            title_data=soup.find("datafield",{"tag":"245","ind1":"1","ind2":tag})
-            soup1=BeautifulSoup(str(title_data),"lxml")
-            title_field=soup1.find("subfield",{"code":"a"})
-            title=title_field.text
-            print(title_field)
-            title=title_field.text
-    #print(title_data)
-    print(title_field.text)'''
-
-    #except:
-        #possible fields: tag="245" ind1="1" ind2="2,3,4,5,6" subfield="a"
-        
-            
-        
+        x_data['issue']=issues
+    try:
+        de640_data=soup.find("datafield",{"tag":"583"})
+        soup4=BeautifulSoup(str(de640_data),"lxml")
+        de640_field=soup4.find("subfield",{"code":"z"})
+        de640=de640_field.text
+        x_data['DE-640']=de640
+        print
+    except:
+        pass
+df=pd.DataFrame(xmldata)
+df.to_csv("./Python/BWLastCopies/test.csv")
     
-    time.sleep(0.1)
+time.sleep(0.1)
